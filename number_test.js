d4c00cc49bd6879feb0d3d6fdd538325d83e27b0
Jeff DeSouza <jeffdesouzadev@gmail.com>
2022-05-16 13:30:58 -0500

    scaffolding for NOUSes

---
+++ number_test.js
@@ -0,0 +1,4 @@
+const NumberOfUnusualSize = require("./numbers_of_unusual_size.js");
+
+let n = new NumberOfUnusualSize(12);
+n.printNumber()
---
+++ numbers_of_unusual_size.js
@@ -0,0 +1,13 @@
+class NumberOfUnusualSize{
+  constructor(initialValue) {
+    this.initialValue = initialValue
+
+  }
+
+  printNumber = function() {
+    console.log(this.initialValue)
+  }
+
+}
+
+module.exports = NumberOfUnusualSize
