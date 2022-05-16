21847301d90bba42174742c1462ebe577babdc12
Jeff DeSouza <jeffdesouzadev@gmail.com>
2022-05-16 14:37:24 -0500

    NOUS equals

--- number_test.js
+++ number_test.js
@@ -1,4 +1,10 @@
 const NumberOfUnusualSize = require("./numbers_of_unusual_size.js");
 
-let n = new NumberOfUnusualSize(12);
-n.printNumber()
+let n = new NumberOfUnusualSize(123456789);
+// n.toString(4)
+let m = new NumberOfUnusualSize(123456780);
+let p = 5
+let q = 123456789
+// console.log(n.equals(m))
+// console.log(n.equals(p))
+ console.log(`${n} compared to ${m}`, n.equals(m))
--- numbers_of_unusual_size.js
+++ numbers_of_unusual_size.js
@@ -1,13 +1,75 @@
 class NumberOfUnusualSize{
   constructor(initialValue) {
-    this.initialValue = initialValue
-
+    const strInitialValue = initialValue.toString()
+    this.numberStore = []
+    for (let k = 0; k < strInitialValue.length; k++) {
+      this.numberStore.push(parseInt(strInitialValue[k]))
+    }
   }
 
-  printNumber = function() {
-    console.log(this.initialValue)
+  toString() {
+    let padding=4
+    let msd = ''
+    let lsd = ''
+    if (padding*2 <= this.numberStore.length) {
+      for (let k = 0; k < this.numberStore.length && k < padding; k++){
+        msd += (this.numberStore[k])
+      }
+      for (let m = this.numberStore.length-padding; m < this.numberStore.length; m++) {
+        lsd += (this.numberStore[m])
+      }
+      console.log(msd + "..." + lsd)
+      return msd + "..." + lsd
+    } else {
+      console.error("number is too small for given padding (consider a regular variable??)");
+    }
+  }
+
+  // toString(padding) {
+  //   let msd = ''
+  //   let lsd = ''
+  //   if (padding*2 <= this.numberStore.length) {
+  //     for (let k = 0; k < this.numberStore.length && k < padding; k++){
+  //       msd += (this.numberStore[k])
+  //     }
+  //     for (let m = this.numberStore.length-padding; m < this.numberStore.length; m++) {
+  //       lsd += (this.numberStore[m])
+  //     }
+  //     console.log(msd + "..." + lsd)
+  //     return msd + "..." + lsd
+  //   } else {
+  //     console.error("number is much too small for given padding (consider a regular variable??)");
+  //   }
+  // }
+
+  equals = function(compareNum) {
+    if (compareNum instanceof NumberOfUnusualSize) {
+      return this.compare(compareNum)
+    } else {
+      if (typeof(compareNum) === "number"){
+        const cn = new NumberOfUnusualSize(compareNum)
+        return this.equals(cn)
+      } else {
+        console.error('Must compare against an int or another NumberOfUnusualSize')
+        console.error("type of ", compareNum, "is ", typeof(compareNum))
+        return false
+      }
+    }
+  }
+
+  compare = function(compareNum) {
+    //compareNum MUST be a NumberOfUnusualSize
+    if (compareNum.numberStore.length !== this.numberStore.length) {
+      return false
+    } else {
+      for (let k = 0; k < this.numberStore.length; k++) {
+        if(this.numberStore[k] !== compareNum.numberStore[k]) {
+          return false
+        }
+      }
+    }
+    return true
   }
 
 }
-
 module.exports = NumberOfUnusualSize
