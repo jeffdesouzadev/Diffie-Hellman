aa7fcb1ca42e7a638c0becccc173ff5fd048e0b9
Jeff DeSouza <jeffdesouzadev@gmail.com>
2022-05-15 21:11:15 -0500

    handshake complete

--- client.js
+++ client.js
@@ -15,41 +15,30 @@ const clientHello = function(keys) {
   return axios(config);
 }
 
+const sendGA = function(ga) {
+  const config = {
+    method: "get",
+    url: "http://localhost:3000/receiveGA",
+    headers: {},
+    params: { ga },
+  };
 
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World')
-})
+  return axios(config);
+}
 
-app.get('/json', (req, res) => {
-  res.status(200).send({'key': 'value'})
-})
+
 
 app.listen(PORT, ()=> {
   console.log(`listening on port ${PORT}`)
 });
 
 const run = async function() {
+
+  const upperBound = 30
   const publicKeys = [
-    89,
-    59,
-    61,
-    53,
-    79,
-    97,
-    83,
-    73,
-    67,
-    71
+     3, 5, 7, 11
   ]
-  let ch = null;
-  try {
-    ch = await clientHello(publicKeys)
-  } catch(err) {
-    console.error('error!', err)
-  }
-  const publicKey = ch.data.key
-  //const publicKey = 71
 
   //note: Since this is for demonstration purposes, I am
   //committing the private key instead of storing in my
@@ -61,9 +50,35 @@ const run = async function() {
   //Diffie Hellman implementation.  Perhaps I'll upgrade
   //this project after this initial exploration of the
   //problem space.
-  const privateKey = 5210189
+  const privateKey = 13
+
+  let ch = null;
+  try {
+    console.log("Offering the server the choice of public key:", publicKeys)
+    ch = await clientHello(publicKeys)
+  } catch(err) {
+    console.error('error!', err)
+  }
+  const publicKey = ch.data.publicKey
+  const gb = ch.data.gb
+
 
   console.log('server-chosen publicKey is: ', publicKey)
+  console.log('publicKey ^ server-secret is', gb)
+
+  const gab = Math.pow(gb, privateKey)
+  console.log('publicKey ^ server ^ client is', gab)
+
+  const mod = gab % upperBound
+  console.log('...and the modulo is ', mod)
+
+  const ga = Math.pow(publicKey, privateKey)
+  try {
+    const send = await sendGA(ga)
+  } catch(err) {
+    console.error('error!', err)
+  }
+
 }
 
 run();
--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const PORT = 3000;
+const upperBound = 30
 let publicKey = undefined
+
+//note: Since this is for demonstration purposes, I am
+//committing the private key instead of storing in my
+//environment.  Please understand that I would never
+//(ever) commit any ACTUAL credentials!
+
+//one other note: I understand that this private key is
+//also much too small to be actually used in any real
+//Diffie Hellman implementation.  Perhaps I'll upgrade
+//this project after this initial exploration of the
+//problem space.
+const privateKey = 11
 
 const serverHello = function() {
   var config = {
@@ -15,8 +28,13 @@ const serverHello = function() {
 
 
 
-app.get('/connection', (req, res) => {
-  //confirm that public key is set
+app.get('/receiveGA', (req, res) => {
+  const ga = req.query.ga
+  console.log('publicKey ^ client-secret is', ga)
+  const mod = Math.pow(ga, privateKey) % upperBound
+  console.log('publicKey ^ client ^ server is', Math.pow(ga, privateKey))
+  console.log('...and the modulo is', mod)
+
   res.status(200).send({publicKey})
 })
 
@@ -24,7 +42,7 @@ app.get('/serverHello', (req, res) => {
   const keys = req.query.keys
   const keyIndex = Math.floor(Math.random()*keys.length)
   publicKey = keys[keyIndex]
-  res.status(200).send({'key': publicKey})
+  res.status(200).send({'publicKey': publicKey, 'gb': Math.pow(publicKey, privateKey)})
 })
 
 app.listen(PORT, ()=> {
